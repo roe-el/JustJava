232b71e142e7b91396593697283a8fc349daad12
roe-el <rogar28@gmail.com>
2016-03-24 11:39:23 -0500

    Add increment and decrement bounds w/ toasts

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -16,7 +17,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-int quantity = 0;
+int quantity = 1;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,12 @@ protected void onCreate(Bundle savedInstanceState) {
  * This method is called when the add button is clicked.
  */
 public void increment(View view) {
-    quantity = 1 + quantity;
+    if (quantity == 100) {
+        //Toast to show
+        Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+        return; //leaves method
+    }
+    quantity += 1;
     displayQuantity(quantity);
 
 }
@@ -37,7 +43,12 @@ public void increment(View view) {
  * This method is called when the minus button is clicked.
  */
 public void decrement(View view) {
-    quantity = quantity - 1;
+    if (quantity == 1) {
+        //Toast to show
+        Toast.makeText(this, "You cannot have less than 1 coffees", Toast.LENGTH_SHORT).show();
+        return; //leaves method
+    }
+    quantity -= 1;
     displayQuantity(quantity);
 
 }
@@ -46,13 +57,17 @@ public void decrement(View view) {
  * This method is called when the order button is clicked.
  */
 public void submitOrder(View view) {
+    //figure out if user wants chocolate topping
     CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+    boolean hasChocolate = chocolateCheckBox.isChecked();
+    //figure out if user wants whipped topping
     CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+    boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+    //Getting name and passing it to createOrderSummary
     EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
     Editable nameOfCustomer = nameEditText.getText();
-    boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-    boolean hasChocolate = chocolateCheckBox.isChecked();
-    String priceMessage = createOrderSummary(nameOfCustomer, calculatePrice(quantity), hasWhippedCream, hasChocolate);
+
+    String priceMessage = createOrderSummary(nameOfCustomer, quantity, calculatePrice(hasWhippedCream, hasChocolate), hasWhippedCream, hasChocolate);
     displayMessage(priceMessage);
 
 }
@@ -60,33 +75,48 @@ public void submitOrder(View view) {
 
 /**
  * Summary of the order
- *@param nameEntered      is the text that was entered by the user
+ *
+ * @param nameEntered     is the text that was entered by the user
  * @param addWhippedCream is whether user wants whipped cream topping
  * @param addChocolate    is whether user wants chocolate topping
- * @param amount          is the price
+ * @param quantity        is the amount of coffes
+ * @param price           is the price
  * @return the message
  */
-private String createOrderSummary(Editable nameEntered, int amount, boolean addWhippedCream, boolean addChocolate) {
+private String createOrderSummary(Editable nameEntered, int quantity, int price, boolean addWhippedCream, boolean addChocolate) {
 
 
     return "Name: " + nameEntered +
-            "\nQuantity: " + amount +
+            "\nQuantity: " + quantity +
             "\nHas Whipped Cream: " + addWhippedCream +
             "\nHas Chocolate: " + addChocolate +
-            "\nTotal: $" + amount +
+            "\nTotal: $" + price +
             "\nThank you!";
+
 
 }
 
 /**
  * Calculates the price of the order.
  *
- * @param quantity is the number of cups of coffee ordered
+ * @param addWhippedCream is if user added whipped cream topping
+ * @param addChocolate    is if user added chocolate topping
  * @return total price
  */
-private int calculatePrice(int quantity) {
+private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+    //Price of 1 cup of coffee
+    int price = 5;
 
-    return quantity * 5;
+    if (addWhippedCream) {
+        //If user wants whipped cream, then $1 is added
+        price += 1;
+    }
+    if (addChocolate) {
+        //If user wants chocolate topping, then $2 is added
+        price += 2;
+    }
+    //Price of total number of cups of coffee with optional topping selected
+    return quantity * (price);
 
 }
 
@@ -104,8 +134,7 @@ private void displayMessage(String message) {
  * @param amount of coffees
  */
 private void displayQuantity(int amount) {
-    TextView quantityTextView = (TextView) findViewById(
-            R.id.quantity_text_view);
+    TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
     quantityTextView.setText("" + amount);
 }
 }
